84be31e9ee58110da833928716d11c9eb6777ddc
Phil Brockman <116004898+phil-loops@users.noreply.github.com>
2022-11-02 11:37:47 -0500

    aoeu

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+describe("111", () => {
+  it("should navigate to the about page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click();
+
+    // The new url should include "/about"
+    cy.url().should("include", "/about");
+
+    // The new page should contain an h1 with "About page"
+    cy.get("h1").contains("About Page");
+
+    // cy.url().should("include", "/BREAKS THE CHAIN");
+  });
+});
+describe("3234", () => {
+  it("should navigate to the about page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click();
+
+    // The new url should include "/about"
+    cy.url().should("include", "/about");
+
+    // The new page should contain an h1 with "About page"
+    cy.get("h1").contains("About Page");
+
+    // cy.url().should("include", "/BREAKS THE CHAIN");
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
